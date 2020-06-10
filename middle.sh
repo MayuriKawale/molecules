b129c3dc68217ea75b691e131f5acb0e5477c65d
Mayuri Kawale <mayuri.kawale-1@ou.edu>
2020-06-10 10:24:47 -0500

    added comments about variable order for script

--- middle.sh
+++ middle.sh
@@ -2,4 +2,7 @@
 #$ is the variable for shell script and the numbering is for the order u can enter variable names
 #added ability to set line numbers
 
+#$1 is filename
+#$2 first set of lines in file
+#$3 last set of lines in file
 head -n "$2" "$1" | tail -n "$3" 
