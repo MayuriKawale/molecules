48f7fed369c2b08f2401c964877301de25abc896
Mayuri Kawale <mayuri.kawale-1@ou.edu>
2020-06-10 10:18:42 -0500

    added the ability to select line numbers

--- middle.sh
+++ middle.sh
@@ -1,3 +1,5 @@
 #Adding the ability to run on any file
-#$ is the variable for shell script
-head -n 15 "$1" | tail -n 5 
+#$ is the variable for shell script and the numbering is for the order u can enter variable names
+#added ability to set line numbers
+
+head -n "$2" "$1" | tail -n "$3" 
